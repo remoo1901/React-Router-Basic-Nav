1dc9918650626077177652c44cf47fd28b2ca19f
remoo1901 <remoo1901@hotmail.com>
2020-01-15 19:50:05 -0500

    step one completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
 
 const App = () => (
   <div>
@@ -9,3 +9,5 @@ const App = () => (
 );
 
 export default App;
+
+
